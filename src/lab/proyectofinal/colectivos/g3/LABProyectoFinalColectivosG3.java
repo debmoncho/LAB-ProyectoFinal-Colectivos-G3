c25700e2a61662323ded2c7435d3e4a0beb0bdf8
debmoncho <elmetalerodelsur@gmail.com>
2024-05-30 21:01:40 -0300

    Initial commit

---
+++ src/lab/proyectofinal/colectivos/g3/LABProyectoFinalColectivosG3.java
@@ -0,0 +1,17 @@
+
+package lab.proyectofinal.colectivos.g3;
+
+/**
+ *
+ * @author Deb APBT
+ */
+public class LABProyectoFinalColectivosG3 {
+
+
+    public static void main(String[] args) {
+        
+        //PROYECTO INICIADO
+        
+    }
+    
+}
