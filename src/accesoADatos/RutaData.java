73c30283fff2a54d43931602587a119aa49162ba
ArielAricoma <ariel16ary@gmail.com>
2024-06-04 21:24:38 -0300

    cambio

--- src/accesoADatos/RutaData.java
+++ src/accesoADatos/RutaData.java
@@ -4,7 +4,16 @@
  */
 package accesoADatos;
 
+import Entidades.Ruta;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.Time;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -14,7 +23,37 @@ public class RutaData {
     private Connection con = null;
     public RutaData(){
     
+        con = Conexion.getConexion();
         
     }
     
+    public void guardarRuta(Ruta ruta){
+    
+        String sql = "INSERT INTO Rutas (origen,destino,duracionEstimada,estado)"
+                + "VALUES (?,?,?,?)";
+        
+        try {
+            PreparedStatement ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            
+            ps.setString(1, ruta.getOrigen());
+            ps.setString(2, ruta.getDestino());
+            ps.setTime(3, Time.valueOf(ruta.getDuracionEstimada()));
+            ps.setBoolean(4, ruta.isEstado());
+            ps.executeUpdate();
+            
+            ResultSet rs = ps.getGeneratedKeys();
+            
+            if(rs.next()){
+            
+                ruta.setIdRuta(rs.getInt(1));
+                JOptionPane.showMessageDialog(null, "Ruta Guardada Correctamente");
+                   
+            }
+            ps.close();
+            
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Ruta Incorrecta");
+        }
+        
+    }
 }
